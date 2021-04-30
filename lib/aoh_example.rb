d17179decc190ecf3609f9faa4f7dcba472b356a
Ian King <kickthechair@gmail.com>
2021-04-30 11:49:35 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -17,23 +17,23 @@ def literal_aoh
   # held in the constants
   array = [
     {
-      name: "Don Gately"
+      name: "Don Gately",
       occupation: "Live-in Staff"
     },
     {
-      name: "Joelle van Dyne"
+      name: "Joelle van Dyne",
       occupation: "Radio Personality"
     },
     {
-      name: "Pat Monteseian"
+      name: "Pat Monteseian",
       occupation: "Staff"
     },
     {
-      name: "Kate Gompert"
+      name: "Kate Gompert",
       occupation: "None"
     },
     {
-      name: "Bruce Green"
+      name: "Bruce Green",
       occupation: "Fan of Mildred"
     },
   ]
